970d182a614b5d6001e715bca7f460981a46c80c
SaekiTominaga <kcsu3470+github@gmail.com>
2026-03-01 17:57:50 +0900

    stylelint-root-colors の required オプションを false にする

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,6 +5,11 @@ export default {
 	extends: ['@w0s/stylelint-config'],
 	rules: {
 		'selector-class-pattern': '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$|^-([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+
+		/**
+		 * [plugin] stylelint-root-colors
+		 */
+		'plugin/root-colors': true,
 	},
 	overrides: [
 		{
