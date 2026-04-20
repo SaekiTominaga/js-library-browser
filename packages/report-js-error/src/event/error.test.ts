e4c3bddffc7dfcc9e64da19488726baf03654304
SaekiTominaga <kcsu3470+github@gmail.com>
2026-04-20 19:20:52 +0900

    Set a timeout for tests containing `fetch`

--- packages/report-js-error/src/event/error.test.ts
+++ packages/report-js-error/src/event/error.test.ts
@@ -11,99 +11,133 @@ const errorEventInit: Readonly<ErrorEventInit> = {
 	colno: 2,
 };
 
-test('正常ケース', async () => {
-	const event = new ErrorEvent('error', errorEventInit);
+const testTimeout = 10000; // default=5000 <https://jestjs.io/docs/api#testname-fn-timeout>
 
-	expect((await errorEvent(event, options))?.status).toBe(204);
-});
+test(
+	'正常ケース',
+	async () => {
+		const event = new ErrorEvent('error', errorEventInit);
 
-test('application/x-www-form-urlencoded', async () => {
-	const event = new ErrorEvent('error', errorEventInit);
+		expect((await errorEvent(event, options))?.status).toBe(204);
+	},
+	testTimeout,
+);
 
-	const fetchOptionsTemp = { ...options.fetch };
-	fetchOptionsTemp.contentType = 'application/x-www-form-urlencoded';
+test(
+	'application/x-www-form-urlencoded',
+	async () => {
+		const event = new ErrorEvent('error', errorEventInit);
 
-	await expect(
-		errorEvent(event, {
-			fetch: fetchOptionsTemp,
-			validate: { ...options.validate },
-		}),
-	).rejects.toThrow('`https://report.w0s.jp/report/js-sample` is 400 Bad Request'); // TODO: データ形式の正当性が確認できていない
-});
+		const fetchOptionsTemp = { ...options.fetch };
+		fetchOptionsTemp.contentType = 'application/x-www-form-urlencoded';
 
-test('fetch error', async () => {
-	const event = new ErrorEvent('error', errorEventInit);
+		await expect(
+			errorEvent(event, {
+				fetch: fetchOptionsTemp,
+				validate: { ...options.validate },
+			}),
+		).rejects.toThrow('`https://report.w0s.jp/report/js-sample` is 400 Bad Request'); // TODO: データ形式の正当性が確認できていない
+	},
+	testTimeout,
+);
 
-	const fetchOptionsTemp = { ...options.fetch };
-	fetchOptionsTemp.endpoint = new URL('https://saekitominaga.github.io/js-library-browser/packages/report-js-error/');
+test(
+	'fetch error',
+	async () => {
+		const event = new ErrorEvent('error', errorEventInit);
 
-	await expect(
-		errorEvent(event, {
-			fetch: fetchOptionsTemp,
-			validate: { ...options.validate },
-		}),
-	).rejects.toThrow('`https://saekitominaga.github.io/js-library-browser/packages/report-js-error/` is 405 Method Not Allowed');
-});
+		const fetchOptionsTemp = { ...options.fetch };
+		fetchOptionsTemp.endpoint = new URL('https://saekitominaga.github.io/js-library-browser/packages/report-js-error/');
+
+		await expect(
+			errorEvent(event, {
+				fetch: fetchOptionsTemp,
+				validate: { ...options.validate },
+			}),
+		).rejects.toThrow('`https://saekitominaga.github.io/js-library-browser/packages/report-js-error/` is 405 Method Not Allowed');
+	},
+	testTimeout,
+);
 
 describe('validate', () => {
 	describe('filename', () => {
-		test('YJApp-ANDROID', async () => {
-			const errorEventInitTemp = { ...errorEventInit };
-			errorEventInitTemp.filename = '';
-
-			const event = new ErrorEvent('error', errorEventInitTemp);
-
-			expect(await errorEvent(event, options)).toBeUndefined();
-		});
-
-		describe('protocol', () => {
-			test('invalid', async () => {
+		test(
+			'YJApp-ANDROID',
+			async () => {
 				const errorEventInitTemp = { ...errorEventInit };
-				errorEventInitTemp.filename = 'ftp://example.com/foo.js';
+				errorEventInitTemp.filename = '';
 
 				const event = new ErrorEvent('error', errorEventInitTemp);
 
 				expect(await errorEvent(event, options)).toBeUndefined();
-			});
+			},
+			testTimeout,
+		);
 
-			test('https', async () => {
-				const errorEventInitTemp = { ...errorEventInit };
-				errorEventInitTemp.filename = 'https://example.com/foo.js';
+		describe('protocol', () => {
+			test(
+				'invalid',
+				async () => {
+					const errorEventInitTemp = { ...errorEventInit };
+					errorEventInitTemp.filename = 'ftp://example.com/foo.js';
 
-				const event = new ErrorEvent('error', errorEventInitTemp);
+					const event = new ErrorEvent('error', errorEventInitTemp);
 
-				expect((await errorEvent(event, options))?.status).toBe(204);
-			});
+					expect(await errorEvent(event, options)).toBeUndefined();
+				},
+				testTimeout,
+			);
+
+			test(
+				'https',
+				async () => {
+					const errorEventInitTemp = { ...errorEventInit };
+					errorEventInitTemp.filename = 'https://example.com/foo.js';
+
+					const event = new ErrorEvent('error', errorEventInitTemp);
+
+					expect((await errorEvent(event, options))?.status).toBe(204);
+				},
+				testTimeout,
+			);
 		});
 
-		test('denys', async () => {
-			const event = new ErrorEvent('error', errorEventInit);
+		test(
+			'denys',
+			async () => {
+				const event = new ErrorEvent('error', errorEventInit);
 
-			expect(
-				await errorEvent(event, {
-					fetch: fetchOptions,
-					validate: {
-						filename: {
-							denys: [/\/foo.js$/v],
+				expect(
+					await errorEvent(event, {
+						fetch: fetchOptions,
+						validate: {
+							filename: {
+								denys: [/\/foo.js$/v],
+							},
 						},
-					},
-				}),
-			).toBeUndefined();
-		});
+					}),
+				).toBeUndefined();
+			},
+			testTimeout,
+		);
 
-		test('allows', async () => {
-			const event = new ErrorEvent('error', errorEventInit);
+		test(
+			'allows',
+			async () => {
+				const event = new ErrorEvent('error', errorEventInit);
 
-			expect(
-				await errorEvent(event, {
-					fetch: fetchOptions,
-					validate: {
-						filename: {
-							allows: [/\/bar.js$/v],
+				expect(
+					await errorEvent(event, {
+						fetch: fetchOptions,
+						validate: {
+							filename: {
+								allows: [/\/bar.js$/v],
+							},
 						},
-					},
-				}),
-			).toBeUndefined();
-		});
+					}),
+				).toBeUndefined();
+			},
+			testTimeout,
+		);
 	});
 });
