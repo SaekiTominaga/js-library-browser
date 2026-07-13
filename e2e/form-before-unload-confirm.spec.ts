56878feb9790c809bf5dab9a10377d13ff59e537
SaekiTominaga <kcsu3470+github@gmail.com>
2026-07-13 11:27:39 +0900

    form-before-unload-confirm のテストを追加

---
+++ e2e/form-before-unload-confirm.spec.ts
@@ -0,0 +1,50 @@
+import { type Dialog, expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ browserName, page }) => {
+	test.skip(['webkit'].includes(browserName), 'Exclude browsers that do not support `beforeunload` event');
+
+	await page.goto('/form-before-unload-confirm/demo/');
+});
+
+test('submit', async ({ page }) => {
+	let calledDialog: Dialog | undefined;
+
+	page.on('dialog', async (dialog) => {
+		await dialog.accept();
+		calledDialog = dialog;
+	});
+
+	await page.getByRole('textbox', { name: 'Type anything' }).first().fill('abc');
+	await page.getByRole('button', { name: 'Submit' }).first().click();
+
+	expect(calledDialog).toBeUndefined();
+});
+
+test.describe('link', () => {
+	test('no input', async ({ page }) => {
+		let calledDialog: Dialog | undefined;
+
+		page.on('dialog', async (dialog) => {
+			await dialog.accept();
+			calledDialog = dialog;
+		});
+
+		await page.getByRole('link', { name: 'example.com' }).first().click();
+
+		expect(calledDialog).toBeUndefined();
+	});
+
+	test('input', async ({ page }) => {
+		let calledDialog: Dialog | undefined;
+
+		page.on('dialog', async (dialog) => {
+			await dialog.accept();
+			calledDialog = dialog;
+		});
+
+		await page.getByRole('textbox', { name: 'Type anything' }).first().fill('abc');
+		await page.getByRole('link', { name: 'example.com' }).first().click();
+
+		expect(calledDialog?.type()).toBe('beforeunload');
+	});
+});
